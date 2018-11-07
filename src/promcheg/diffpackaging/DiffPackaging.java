14600798c2926408d86da66235118cdcd46159e9
wamu <waldemar.mueller-ext@idemtelematics.com>
2018-11-07 22:06:08 +0100

    work in progress

--- src/promcheg/diffpackaging/DiffPackaging.java
+++ src/promcheg/diffpackaging/DiffPackaging.java
@@ -3,7 +3,11 @@ package promcheg.diffpackaging;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class DiffPackaging {
 	public static final String[] RELEVANT_FILES = {".cs", ".aspx", ".html"};
@@ -46,27 +50,82 @@ public class DiffPackaging {
 	
 	private void process() {
 		ArrayList<String> initalFileList = readFileList(initialDirectory);
+		ArrayList<String> currentFileList = readFileList(currentDirectory);
+		ArrayList<String> outputFileList = new ArrayList<>();
+		Optional<String> initialFirstFile = initalFileList.stream().sorted().findFirst();
+		Optional<String> currentFirstFile = currentFileList.stream().sorted().findFirst();
 		
-		System.out.println(initialDirectory);
-		System.out.println(currentDirectory);
-		System.out.println(outpuDirectory);
+		//trim base directory
+		AtomicReference<String> initialBaseDirectory = new AtomicReference<String>("");
+		AtomicReference<String> currentBaseDirectory = new AtomicReference<String>("");
+		String separator = "\\";
+		String[] initialDirectoryStruct = initialFirstFile.get().replaceAll(Pattern.quote(separator), "/").split("/");
+		String[] currentDirectoryStruct = currentFirstFile.get().replaceAll(Pattern.quote(separator), "/").split("/");
+		int projectRootOffsetIdx = -1;
+		for(int i = 1; i <= initialDirectoryStruct.length; i++) {
+			if(		projectRootOffsetIdx == -1 && 
+					!initialDirectoryStruct[initialDirectoryStruct.length-i]
+							.equalsIgnoreCase(
+									currentDirectoryStruct[currentDirectoryStruct.length-i])) {
+				projectRootOffsetIdx = i;
+			}
+			
+			if(projectRootOffsetIdx != -1) {
+				initialBaseDirectory.set(initialDirectoryStruct[initialDirectoryStruct.length- i+1] + separator + initialBaseDirectory.get());
+				if(currentDirectoryStruct.length-i+1 >= 0) {
+					currentBaseDirectory.set(currentDirectoryStruct[currentDirectoryStruct.length-i+1] + separator + currentBaseDirectory.get());
+				}
+			}
+		}
+		initialBaseDirectory.set(initialDirectoryStruct[0]+separator+initialBaseDirectory.get());
+
+		initalFileList.stream().forEach(absoluteFileName->{
+			String initialFileName = absoluteFileName.substring(initialBaseDirectory.get().length());
+			AtomicReference< String> currentAbsoluteFileName = new AtomicReference<String>();
+			for(String fileName : currentFileList) {
+				if(fileName.endsWith(initialFileName)) {
+					currentAbsoluteFileName.set(fileName);
+					break;
+				}
+			}
+			
+			if(currentAbsoluteFileName.get() == null || isModified(absoluteFileName, currentAbsoluteFileName.get())) {
+				outputFileList.add(currentAbsoluteFileName.get());
+			}
+		});
+	}
+
+	private boolean isModified(String originalFile, String currentFile) {
+		return false;
 	}
 
 	private ArrayList<String> readFileList(String directory) {
 		ArrayList<String> result = new ArrayList<>();
-		
+		if(directory == null) {
+			return result;
+		}
 		File dir = new File(directory);
 		if(dir.isDirectory()) {
 			Arrays.stream(dir.listFiles()).forEach(file->{
 				if(file.isDirectory()) {
 					ArrayList<String> subFiles = readFileList(file.getAbsolutePath());
 					result.addAll(subFiles);
-				} else {
+				} else if(isAllowed(file.getName())){
 					result.add(file.getAbsolutePath());
 				}
 			});
 		}
 		return result;
+	}
+
+	private boolean isAllowed(String name) {
+		AtomicBoolean found  = new AtomicBoolean(false);
+		Arrays.stream(RELEVANT_FILES).forEach(ext->{
+			if(!found.get() && name.endsWith(ext)) {
+				found.set(true);
+			}
+		});
+		return found.get();
 	}
 
 	protected static void printHelp(String errorMessage) {
